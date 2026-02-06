254b143c9f7074c967739e16861a8ec8d0be5bc3
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-02-06 21:30:33 +0200

    update Day 6

---
+++ Day6OOPDemoLab/PartialClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Day6OOPDemoLab
+{
+    public partial class PartialClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("This is Method 1");
+        }
+    }
+}
