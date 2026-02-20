31e950f99466ccfacfc93f185a0bbb9d6246f680
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-02-21 00:33:05 +0200

    Update

---
+++ HTML5Day3Demos/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTML5Day3Demos
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string password, string username)
+        {
+            return View();
+        }
+    }
+}
