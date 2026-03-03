ae9e3bfa0f73d98e114a095eb89d9a4244cfa9e7
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2026-03-04 00:34:40 +0200

    Update

---
+++ CsharpNameSpaceDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Day8DLLDemo;
+
+namespace CsharpNameSpaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee e = new Employee()
+            { ID = 1, Name = "John", Job = "Developer", Salary = 50000, Attend = true };
+
+        }
+    }
+}
+
+namespace HelpCode
+{
+    public class HelpClass
+    {
+        public void HelpMethod()
+        {
+            Console.WriteLine("This is a help method.");
+        }
+    }
+}
